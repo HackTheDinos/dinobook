9035cdaca151a0157df91fa50e7a8bbca57aab33
Nicole <nhb228@nyu.edu>
2015-11-21 18:25:32 -0500

    schemaaaa

--- Dinos/lib/collections.js
+++ Dinos/lib/collections.js
@@ -1,2 +1,40 @@
 Tasks = new Meteor.Collection('tasks');
 Fossils = new Meteor.Collection('fossils');
+
+/* Fossils Schema */
+
+var Schema = {};
+
+Schema.Fossil = new SimpleSchema({
+	specNum: {
+		type: String,
+		label: "Specimen Number",
+	},
+	locality: {
+		type: String,
+		label: "Locality"
+	},
+	components: {
+		type: Array,
+		label: "Components"
+		optional: true
+	},
+	"components.$": {
+		type: String
+	}
+	date: {
+		type: Date,
+		label: "Date Collected"
+	},
+	collector: {
+		type: String,
+		label: "Collector"
+	},
+	media: {
+		type: String,
+		label: "Temp field for photo/vid",
+		optional: true
+	}
+});
+
+Fossils.attachSchema(Schema.Fossil);
