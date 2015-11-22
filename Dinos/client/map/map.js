31d9d09874066b711e3b09ea4b6c49144d2cd412
Nicole <nhb228@nyu.edu>
2015-11-22 03:13:52 -0500

    hey map hey

--- Dinos/client/map/map.js
+++ Dinos/client/map/map.js
@@ -1,32 +1,55 @@
 
-  Meteor.startup(function() {
-    GoogleMaps.load();
-  });
+Meteor.startup(function() {
+  GoogleMaps.load();
+});
 
 
 Template.map.onRendered(function() {
   GoogleMaps.load();
 });
 
+Template.map.events = {
+  'submit #searchCenter': function (evt, template) {
+    evt.preventDefault();
+    var inputCoordinates = (template.find(".searchCenterInput").value);
+    var coords = inputCoordinates.split(',');
+    var inputCoordsX = coords[0];
+    var inputCoordsY = coords[1];
+
+    if (isInt(inputCoordsX) && isInt(inputCoordsY)) {
+      console.log('hi');
+   /*   if (GoogleMaps.loaded()) {
+      // Map initialization options
+        new google.maps.Map(document.getElementById(''), {
+          zoom:8,
+          center: P
+        })
+        zoom: 8
+      };*/
+    }
+  }
+}
+
 Template.map.helpers({
   exampleMapOptions: function() {
     // Make sure the maps API has loaded
-    
     var themFossilz = Fossils.find().fetch();    
     var lastFossil = (themFossilz.length)-1;
-    var centerCoordinatex = themFossilz[lastFossil].coordinates.x; 
 
-    var centerCoordinatey = themFossilz[lastFossil].coordinates.y;
-
-    if (GoogleMaps.loaded()) {
+    if (lastFossil >= 0) {
+      var centerCoordinatex = themFossilz[lastFossil].coordinates.x; 
+      var centerCoordinatey = themFossilz[lastFossil].coordinates.y;
+    }
+    else { 
+      centerCoordinatex == 0; 
+      centerCoordinatey == 0;
+    }
       // Map initialization options
       return {
-    //    center: new google.maps.LatLng(-37.8136, 144.9631),        
-    center: new google.maps.LatLng(centerCoordinatex, centerCoordinatey),
-    zoom: 8
-  };
-}
-}
+        center: new google.maps.LatLng(centerCoordinatex, centerCoordinatey),
+        zoom: 8
+      };
+    }
 });
 
 Template.map.onCreated(function() {
@@ -46,11 +69,14 @@ Template.map.onCreated(function() {
         position: fossilLat,
         title: themFossilz[i].specNum.toString(),
         map: map.instance,
-        animation: google.maps.Animation.DROP
       })
-      addInfoBox( fossilMarker, themFossilz[i] );
+      addInfoBox( fossilMarker, themFossilz[i], marker.map );
     }
-
+  
+    google.maps.event.addDomListener('outer', 'submit #searchCenter', function(result) {
+      console.log(result);
+      console.log('hey');
+    });
   });
 });
 
@@ -62,17 +88,26 @@ function getComponents(fossilComponents) {
   return componentString
 }
 
-function addInfoBox(marker, fossil) {
+function isInt(value) {
+  return !isNaN(value) && 
+  parseInt(Number(value)) == value && 
+  !isNaN(parseInt(value, 10));
+}
+
+function addInfoBox(marker, fossil, map) {
   var infowindow = new google.maps.InfoWindow({
     content: (
       "Specimen Number: " + fossil.specNum + "<br />" +
       "Collector: " + fossil.collector + "<br />" +
-      "Date Collected: " + fossil.date + "<br /."  +
+      "Date Collected: " + fossil.date + "<br /"  +
       "Components: " + getComponents(fossil.components)
       )
-    })
+  })
 
-    marker.addListener('click', function() {
-      infowindow.open(marker.get('map'), marker)
-    })
-  } 
+  marker.addListener('click', function() {
+    infowindow.open(marker.get('map'), marker);
+    //  var currentZoom = map.zoom;
+    //  map.setZoom(currentZoom + 1);
+    map.setCenter(marker.getPosition());
+  })
+} 
